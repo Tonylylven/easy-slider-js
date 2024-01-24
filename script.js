b58dcb6d8ff2bb6a3a5d668a43ea072b4be2831b
Forsek-beep <romanionin075@gmail.com>
2024-01-24 22:28:01 +0300

    upd

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const slider = document.querySelector('.slider');
 const nextButton = document.querySelector('.next');
 const prevButton = document.querySelector('.prev');
 
-let counter = 0;
+let i = 0;
 const slideWidth = 500;
 
 nextButton.addEventListener('click', () => {
-  counter = (counter + 1) % 8; 
-  slider.style.transform = `translateX(${-slideWidth * counter}px)`; 
+  i = (i + 1) % 8; 
+  slider.style.transform = `translateX(${-slideWidth * i}px)`; 
 });
 
 prevButton.addEventListener('click', () => {
-  counter = (counter - 1 + 8) % 8;
-  slider.style.transform = `translateX(${-slideWidth * counter}px)`; 
+  i = (i - 1 + 8) % 8;
+  slider.style.transform = `translateX(${-slideWidth * i}px)`; 
 });
